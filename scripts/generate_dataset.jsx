32da9cfc75817d1c976a129ec87063167a0a5a12
len0rd <tylerm15@gmail.com>
2018-11-02 12:24:37 -0600

    add mac install script for my sake. working on generate script

--- scripts/generate_dataset.jsx
+++ scripts/generate_dataset.jsx
@@ -1,18 +1,27 @@
-// In order to see this in File > Scripts menu, place in Photoshops Presets/Scripts folder
+// In order to see this in File > Scripts menu, place in Photoshop's Presets/Scripts folder
 
 var dim = 40 // dimension of sides for the square image
 
 // set script units to pixels:
 preferences.rulerUnits = Units.PIXELS
+var baseDoc = app.documents.add(dim, dim) // create new document
 
-
-// create 30x30 document
-var baseDoc = app.documents.add(dim, dim)
-
+// setup the text layer
 var textLayer = baseDoc.artLayers.add()
 textLayer.kind = LayerKind.TEXT
-
-
+baseDoc.layers[1]
 var text = textLayer.textItem
-text.contents = "W"
-text.size = 24
+text.size = 36
+
+text.contents = "A"
+
+
+var bounds = textLayer.bounds
+// bound[0], bound[1] is the top left corner
+// -bounds resets image to top left corner
+// -bounds[0] + dim/2 places left edge in center
+// (bounds[2] - bounds[0]) / 2 figures out the center of the content in the layer (ie: the letter)
+//      and subtracts that so the center of the layer content is in the center of the document
+var xTranslate = (-bounds[0] + (dim / 2)) - ((bounds[2] - bounds[0]) / 2)
+var yTranslate = (-bounds[1] + (dim / 2)) - ((bounds[3] - bounds[1]) / 2)
+textLayer.translate(xTranslate, yTranslate)
---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cp -v generate_dataset.jsx /Applications/Adobe\ Photoshop\ CC\ 2018/Presets/Scripts
+fi
