40e36b67663527e55434b5186829a92f853e4846
len0rd <tylerm15@gmail.com>
2018-11-02 18:43:30 -0600

    generates rotated image files for all characters

--- scripts/generate_dataset.jsx
+++ scripts/generate_dataset.jsx
@@ -1,27 +1,41 @@
 // In order to see this in File > Scripts menu, place in Photoshop's Presets/Scripts folder
 
-var dim = 40 // dimension of sides for the square image
+const DIM_ = 40; // dimension of sides for the square image
+const ROTATE_STEP_ = 5; //degrees to rotate the letter for each image
+const BASE_SAVE_DIR_ = '~/Desktop/Generated/'; //base directory to save into
 
 // set script units to pixels:
-preferences.rulerUnits = Units.PIXELS
-var baseDoc = app.documents.add(dim, dim) // create new document
+preferences.rulerUnits = Units.PIXELS;
+var baseDoc = app.documents.add(DIM_, DIM_); // create new document
 
 // setup the text layer
-var textLayer = baseDoc.artLayers.add()
-textLayer.kind = LayerKind.TEXT
-baseDoc.layers[1]
-var text = textLayer.textItem
-text.size = 36
+var textLayer = baseDoc.artLayers.add();
+textLayer.kind = LayerKind.TEXT;
+baseDoc.layers[1];
+var text = textLayer.textItem;
+text.size = 36;
 
-text.contents = "A"
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+for (var i = 0; i < alphabet.length; i++) {
+    text.contents = alphabet[i];
 
+    // center::
+    var bounds = textLayer.bounds; //If the font isn't monospaced, our bounds will change with each letter
+    // bound[0], bound[1] is the top left corner
+    // -bounds resets image to top left corner
+    // -bounds[0] + DIM_/2 places left edge in center
+    // (bounds[2] - bounds[0]) / 2 figures out the center of the content in the layer (ie: the letter)
+    //      and subtracts that so the center of the layer content is in the center of the document
+    var xTranslate = (-bounds[0] + (DIM_ / 2)) - ((bounds[2] - bounds[0]) / 2);
+    var yTranslate = (-bounds[1] + (DIM_ / 2)) - ((bounds[3] - bounds[1]) / 2);
+    textLayer.translate(xTranslate, yTranslate);
 
-var bounds = textLayer.bounds
-// bound[0], bound[1] is the top left corner
-// -bounds resets image to top left corner
-// -bounds[0] + dim/2 places left edge in center
-// (bounds[2] - bounds[0]) / 2 figures out the center of the content in the layer (ie: the letter)
-//      and subtracts that so the center of the layer content is in the center of the document
-var xTranslate = (-bounds[0] + (dim / 2)) - ((bounds[2] - bounds[0]) / 2)
-var yTranslate = (-bounds[1] + (dim / 2)) - ((bounds[3] - bounds[1]) / 2)
-textLayer.translate(xTranslate, yTranslate)
+    // rotate around
+    var angle = 0; //keeps track of how far we're rotated
+    do {
+        baseDoc.saveAs(new File(BASE_SAVE_DIR_ + alphabet[i] + "-" + angle + ".jpeg"));
+        angle += ROTATE_STEP_;
+        textLayer.rotate(ROTATE_STEP_, AnchorPosition.MIDDLECENTER);
+    } while (angle <= 360);
+
+}
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     cp -v generate_dataset.jsx /Applications/Adobe\ Photoshop\ CC\ 2018/Presets/Scripts
+    rm -rf ~/Desktop/Generated/
+    mkdir -p ~/Desktop/Generated
 fi
