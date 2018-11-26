c44a7b9305ae12db89b7620b4865a012893f10e7
len0rd <tylerm15@gmail.com>
2018-11-26 11:29:54 -0700

    simple.py is working. got 34% accuracy

--- simple.py
+++ simple.py
@@ -124,10 +124,11 @@ with torch.no_grad():
         outputs = net(images)
         _, predicted = torch.max(outputs, 1)
         c = (predicted == labels).squeeze()
-        for i in range():
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
+        if c.size():
+            for i in range(len(predicted)):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
 
 
 for i in range(len(classes)):
