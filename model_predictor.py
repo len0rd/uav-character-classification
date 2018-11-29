5864d422c3e7885bbeb9c478a07be875167d79a3
len0rd <tylerm15@gmail.com>
2018-11-29 11:20:53 -0700

    model_predictor can predict the classification of a single image

--- model_predictor.py
+++ model_predictor.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from PIL import Image
+import PIL# import Image
 from torchvision import transforms
 from torch.autograd import Variable
 
@@ -9,14 +9,17 @@ alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 input_size = 224
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 loader = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3), # this needs to go first. expecting pil image
     transforms.Resize(input_size),
     transforms.CenterCrop(input_size),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
 def loadImage(imgPath):
-    img = Image.open(imgPath)
-    img = loader(img).float()
+    print('path:: {}'.format(type(imgPath)))
+    pilImg = PIL.Image.open(imgPath)
+    print('img:: {}'.format(type(pilImg)))
+    img = loader(pilImg).float()
     img = Variable(img, requires_grad=False)
     img = img.unsqueeze(0)
     img = img.to(device)
@@ -28,7 +31,7 @@ model = model.to(device)
 model.eval()
 print("Model loaded and set to eval mode")
 
-loadedImg = loadImage('sher-target.jpg')
+loadedImg = loadImage('dataset/train/D/D-15-4.jpg')
 print('Loaded image!')
 with torch.set_grad_enabled(False):
     outputs = model(loadedImg)
